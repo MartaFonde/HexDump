37db274bec0cf57b2ece2863d6096e8c29e5075e
Marta Fondevila <martafondevila8@gmail.com>
2020-06-06 23:41:15 +0200

    Prueba2

--- src/app/conversorHexad.java
+++ src/app/conversorHexad.java
@@ -1,7 +1,6 @@
 package app;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class conversorHexad {
     public static void leerHex(String hexa) {
@@ -15,11 +14,9 @@ public class conversorHexad {
         for (int i = 1; i <= hexa.length(); i++) {
             if (nuevaFila) {
                 ceros = 7 - (Integer.toHexString(hexFila).length());
-
-                if (Integer.toHexString(hexFila).length() == 2) {
-                    ceros++;
+                if (Integer.toHexString(hexFila).length() > 1) {
+                    ceros+= Integer.toHexString(hexFila).length()-1;
                 }
-
                 fila = String.format("%" + ceros + "s", Integer.toHexString(hexFila));
                 filaFormat = fila.replace(' ', '0');
                 System.out.printf("%s%d  ", filaFormat, 0);
@@ -41,7 +38,6 @@ public class conversorHexad {
                 loQueQueda = hexa.substring(i, hexa.length());
             }
         }
-
         System.out.println();
         System.out.printf("%s%s", filaFormat, (Integer.toHexString((loQueQueda.length()) / 2)));
     }
@@ -49,28 +45,23 @@ public class conversorHexad {
     public static void main(String[] args) throws Exception {
         if (args.length > 0) {
             File archivo = new File(args[0]);
-            String texto;
-            String converChar;
+            String conversion;
             String hexa = "";
             if (args.length == 1) {
                 if (archivo.exists()) {
-                    try (Scanner f = new Scanner(archivo)) {
-                        while (f.hasNext()) {
-                            texto = f.nextLine();
-                            for (int i = 0; i < texto.length(); i++) {
-                                converChar = Integer.toHexString(texto.charAt(i));
-                                if (converChar.length() == 1) {
-                                    converChar = "0" + converChar;
-                                }
-                                hexa = hexa.concat(converChar);
+                    try (RandomAccessFile f = new RandomAccessFile(archivo, "r")) {
+                        for (int i = 0; i < f.length(); i++) {
+                            f.seek(i);
+                            conversion = Integer.toHexString(f.readByte());
+                            if (conversion.length() == 1) {
+                                conversion = '0' + conversion;
                             }
-                            hexa = hexa.concat("0" + Integer.toHexString('\n'));
+                            hexa = hexa.concat(conversion);
                         }
                         leerHex(hexa);
                         System.out.println();
-
-                    } catch (IOException e) {
-                        System.out.println("Error de lectura: " + e.getMessage());
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Error al abrir archivo: " + e.getMessage());
                     }
                 }
             }
